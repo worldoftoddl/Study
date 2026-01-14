e86669b0d30dd46b2e0eec7fb938dbf0255e1adf
신원석 <won_99@naver.com>
2026-01-14 17:44:34 +0900

    모듈화 및 추상화

--- LangChain/RAG Application 개발/llm.py
+++ LangChain/RAG Application 개발/llm.py
@@ -6,39 +6,55 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain_core.prompts import ChatPromptTemplate
 
 
-def get_ai_message(user_message):
+def get_claude(model='claude-3-haiku-20240307', temperature=0.05, top_p=1, max_tokens=None):
+    claude = ChatAnthropic(
+        model= model,
+        temperature= temperature,
+        top_p= top_p,
+        max_tokens= max_tokens
+        # timeout=,
+        # max_retries=,
+        )
+    return claude
+  
 
-    embedding = UpstageEmbeddings(model='embedding-passage')
-    index_name = 'tax-index-upstage'
-    database = PineconeVectorStore.from_existing_index(index_name=index_name, embedding=embedding)
-
-    llm = ChatAnthropic(
-        model='claude-3-haiku-20240307',
-        temperature=0.05,
-        top_p=1
-        # # max_tokens=,
-        # # timeout=,
-        # # max_retries=,
-    )
-
-    retriever = database.as_retriever(
-        search_kwargs={'k': 20, 'fetch_k': 50}
-    )
+def get_dictionary_chain(llm=None, dictionary=None):
+    if llm is None:
+        llm = get_claude()
+    if dictionary is None:
+        dictionary = ['사람을 나타내는 표현 -> 거주자']
     
-        
-    # 사전 정의
-    dictionary = ['사람을 나타내는 표현 -> 거주자']
-    kward_prompt = ChatPromptTemplate.from_template(f'''                                              
-        사용자의 질문 중 우리의 사전을 참고하여 사용자의 질문 속 특정 어휘만을 수정해주세요.
-        만일 변경할 필요가 없다고 판단된다면 입력된 질문을 그대로 반환해주세요.
-        ---
-        사전: {dictionary}
-        질문: {{question}} 
-    ''')      
+    keyward_prompt = ChatPromptTemplate.from_template(
+      f'''
+      사용자의 질문 중 우리의 사전을 참고하여 사용자의 질문 속 특정 어휘만을 수정해주세요.
+      만일 변경할 필요가 없다고 판단된다면 입력된 질문을 그대로 반환해주세요.
+      ---
+      사전: {dictionary}
+      질문: {{question}} 
+      ''')      
     
     # 용어 필터 chain 만들기
-    dict_chain = kward_prompt | llm | StrOutputParser()
+    dict_chain = keyward_prompt | llm | StrOutputParser()
+    return dict_chain
 
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
+def get_retriever(index_name=None, embedding=None, k=20, fetch_k=50):
+    if index_name is None:
+        index_name = 'tax-index-upstage'
+    if embedding is None:
+        embedding = UpstageEmbeddings(model='embedding-passage')
+
+    database = PineconeVectorStore.from_existing_index(index_name=index_name, embedding=embedding)
+
+    retriever = database.as_retriever(search_kwargs={'k': k, 'fetch_k': fetch_k})
+    return retriever
+
+
+def get_tax_prompt():
     rag_prompt = ChatPromptTemplate.from_messages([
         ("system", """다음 context를 바탕으로 질문에 답하세요.
         당신은 대한민국 세법 전문가입니다. 다음 Context를 바탕으로 사용자의 질문에 대해 명확하고 정확하게 답변해주세요.
@@ -55,15 +71,25 @@ def get_ai_message(user_message):
         
         Let's think step by step
         
-        \n\nContext: {context}"""),
+        ---
+        Context: {context}"""),
         ("human", "{question}")
     ])
+    return rag_prompt
 
-    def format_docs(docs):
-        return "\n\n".join(doc.page_content for doc in docs)
 
+def get_ai_message(user_message, llm=None, rag_prompt=None, dictionary_chain=None):    
+    retriever = get_retriever()
+    if llm is None:
+        llm = get_claude()    
+    if dictionary_chain is None:
+        dictionary_chain = get_dictionary_chain()
+    if rag_prompt is None:
+        rag_prompt = get_tax_prompt()
+
+    
     tax_qa_chain = (
-        dict_chain |
+        dictionary_chain |
         {
             "context": retriever | format_docs,
             "question": RunnablePassthrough(),
